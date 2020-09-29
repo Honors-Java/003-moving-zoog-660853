39d76ec139250bc2e2347400ea54796c986ce768
660853 <jj2497@comcast.net>
2020-09-29 14:14:19 +0000

    added colors.

--- index.java
+++ index.java
@@ -14,14 +14,14 @@ void draw() {
   rectMode(CENTER);
   // Draw Zoog's body
   stroke(0);
-  fill(150);
+  fill(150,mouseX,100);
   rect(mouseX, mouseY,20,100);
   // Draw Zoog's head
   stroke(0);
-  fill(255);
+  fill(mouseX,mouseY,75);
   ellipse(mouseX, mouseY - 30,60,60);
   // Draw Zoog's eyes
-  fill(0);
+  fill(mouseX,mouseY,200);
 
   // remove the forward slashes in front of each line of code below one at a time. Remove the underline and replace with the proper code to allow your mouse to move Zoog around  💻
 
@@ -29,6 +29,6 @@ void draw() {
   ellipse(mouseX+15,mouseY-28,16,32);
   // Draw Zoog's legs
   stroke(0);
-  line(mouseX-10, mouseY+50, 150, 200);
-  line(mouseX+10, mouseY+50, 165, 200);
+  line(mouseX-10, mouseY+50, mouseX-20,mouseY+70);
+  line(mouseX+10, mouseY+50, mouseX+20,mouseY+70);
 }
